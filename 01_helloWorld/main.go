4fbf369ac733fa764ebe5de5512d2dfff07fd8d7
Ron Jawanda <mobilessbiz@gmail.com>
2018-03-25 13:21:29 -0700

    Fixed invalid fmt statement.

--- 01_helloWorld/main.go
+++ 01_helloWorld/main.go
@@ -5,5 +5,5 @@ import "fmt"
 func main() {
 	fmt.Println("Hello World")
 	fmt.Println("This is a second commit")
-	fmt.Println("this is a third commit from go language tutorial"
+	fmt.Println("this is a third commit from go language tutorial")
 }
